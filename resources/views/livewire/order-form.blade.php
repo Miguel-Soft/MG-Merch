c3b0e84e5153a80099dea31a6f2fb876e049d07c
MiguelBilliet1995 <miguel.billiet@student.ehb.be>
2022-04-23 15:15:30 +0200

    Closing form

--- resources/views/livewire/order-form.blade.php
+++ resources/views/livewire/order-form.blade.php
@@ -54,11 +54,11 @@
                             @enderror
                         </div>
 
-                        <label for="toggle-example" class="flex relative items-center mb-4 cursor-pointer">
+                        {{-- <label for="toggle-example" class="flex relative items-center mb-4 cursor-pointer">
                             <input type="checkbox" id="toggle-example" class="sr-only" wire:model="bbq">
                             <div class="w-11 h-6 bg-gray-200 rounded-full border border-gray-200 toggle-bg dark:bg-gray-700 dark:border-gray-600"></div>
                             <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Ik wens deel te nemen aan de BBQ</span>
-                        </label>
+                        </label> --}}
             
                     </div>
 
